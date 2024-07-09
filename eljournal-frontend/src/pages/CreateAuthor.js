96441dcf9e320d9d0095050d1ffe0ed036cbef0d
amineBenFraj <mohamedamine.benfredj@polytechnicien.tn>
2024-07-09 11:59:06 +0100

    create fix

--- eljournal-frontend/src/pages/CreateAuthor.js
+++ eljournal-frontend/src/pages/CreateAuthor.js
@@ -29,18 +29,22 @@ export default function CreateAuthor() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" value={authorForm.name} onChange={handleChange} />
+        <div className=" max-w-[600px] bg-gradient-to-r from-gray-900 via-gray-900 to-gray-900 border border-transparent rounded-lg shadow-lg p-8">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="name" className="text-gray-400 font-semibold text-sm">Name:</label>
+                    <input type="text" id="name" name="name" value={authorForm.name} onChange={handleChange} required
+                        className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 placeholder-opacity-50" />
                 </div>
-                <div>
-                    <label htmlFor="bio">Bio:</label>
-                    <textarea id="bio" name="bio" value={authorForm.bio} onChange={handleChange}></textarea>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="bio" className="text-gray-400 font-semibold text-sm">Bio:</label>
+                    <textarea id="bio" name="bio" value={authorForm.bio} onChange={handleChange} required
+                        className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 resize-none placeholder-opacity-50"></textarea>
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit"
+                    className="flex items-center justify-center self-start font-semibold text-gray-400 bg-gray-800 border border-gray-600 px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-white hover:text-gray-800">Submit</button>
             </form>
         </div>
+
     );
 }
