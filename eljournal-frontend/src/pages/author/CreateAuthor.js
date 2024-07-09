6bcc71451be38a844864dd2391074e22c39d9994
amineBenFraj <mohamedamine.benfredj@polytechnicien.tn>
2024-07-09 16:37:44 +0100

    styling a bit

--- eljournal-frontend/src/pages/author/CreateAuthor.js
+++ eljournal-frontend/src/pages/author/CreateAuthor.js
@@ -29,38 +29,40 @@ export default function CreateAuthor() {
     }
 
     return (
-        <div className="max-w-[600px] bg-gradient-to-r from-gray-900 via-gray-900 to-gray-900 border border-transparent rounded-lg shadow-lg p-8">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="name" className="text-gray-400 font-semibold text-sm">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={authorForm.name}
-                        onChange={handleChange}
-                        required
-                        className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 placeholder-opacity-50"
-                    />
-                </div>
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="bio" className="text-gray-400 font-semibold text-sm">Bio:</label>
-                    <textarea
-                        id="bio"
-                        name="bio"
-                        value={authorForm.bio}
-                        onChange={handleChange}
-                        required
-                        className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 resize-none placeholder-opacity-50"
-                    ></textarea>
-                </div>
-                <button
-                    type="submit"
-                    className="flex items-center justify-center self-start font-semibold text-gray-400 bg-gray-800 border border-gray-600 px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-white hover:text-gray-800"
-                >
-                    Submit
-                </button>
-            </form>
+        <div className="flex items-center justify-center h-screen">
+            <div className="max-w-2xl w-full bg-gradient-to-r from-gray-900 via-gray-900 to-gray-900 border border-transparent rounded-lg shadow-lg p-8">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="name" className="text-gray-400 font-semibold text-sm">Name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={authorForm.name}
+                            onChange={handleChange}
+                            required
+                            className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 placeholder-opacity-50"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="bio" className="text-gray-400 font-semibold text-sm">Bio:</label>
+                        <textarea
+                            id="bio"
+                            name="bio"
+                            value={authorForm.bio}
+                            onChange={handleChange}
+                            required
+                            className="px-4 py-2 bg-transparent border border-gray-600 rounded-lg text-white focus:outline-none focus:border-purple-500 resize-none placeholder-opacity-50"
+                        ></textarea>
+                    </div>
+                    <button
+                        type="submit"
+                        className="flex items-center justify-center self-start font-semibold text-gray-400 bg-gray-800 border border-gray-600 px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-white hover:text-gray-800"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
